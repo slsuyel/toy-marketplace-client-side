bba5b4cca8dbb519528fad4ef0cbb192ce2b79bf
Suyel Haque <slsuyel@gmail.com>
2023-05-17 21:37:16 +0600

    footer-edit

--- src/pages/shares/Footer/Footer.jsx
+++ src/pages/shares/Footer/Footer.jsx
@@ -11,7 +11,7 @@ const Footer = () => {
                         </ul>
 
                     </div>
-                    <div className="col-6 col-lg-2 offset-lg-1 mb-3">
+                    <div className="col-6 col-lg-3 mb-3">
                         <h5>Links</h5>
                         <ul className="list-unstyled">
                             <li className="mb-2"><a href="/">Home</a></li>
@@ -23,27 +23,8 @@ const Footer = () => {
                             <li className="mb-2"><a href="https://cottonbureau.com/people/bootstrap">Swag Store</a></li>
                         </ul>
                     </div>
-                    <div className="col-6 col-lg-2 mb-3">
-                        <h5>Guides</h5>
-                        <ul className="list-unstyled">
-                            <li className="mb-2"><a href="/docs/5.3/getting-started/">Getting started</a></li>
-                            <li className="mb-2"><a href="/docs/5.3/examples/starter-template/">Starter template</a></li>
-                            <li className="mb-2"><a href="/docs/5.3/getting-started/webpack/">Webpack</a></li>
-                            <li className="mb-2"><a href="/docs/5.3/getting-started/parcel/">Parcel</a></li>
-                            <li className="mb-2"><a href="/docs/5.3/getting-started/vite/">Vite</a></li>
-                        </ul>
-                    </div>
-                    <div className="col-6 col-lg-2 mb-3">
-                        <h5>Projects</h5>
-                        <ul className="list-unstyled">
-                            <li className="mb-2"><a href="https://github.com/twbs/bootstrap">Bootstrap 5</a></li>
-                            <li className="mb-2"><a href="https://github.com/twbs/bootstrap/tree/v4-dev">Bootstrap 4</a></li>
-                            <li className="mb-2"><a href="https://github.com/twbs/icons">Icons</a></li>
-                            <li className="mb-2"><a href="https://github.com/twbs/rfs">RFS</a></li>
-                            <li className="mb-2"><a href="https://github.com/twbs/examples/">Examples repo</a></li>
-                        </ul>
-                    </div>
-                    <div className="col-6 col-lg-2 mb-3">
+
+                    <div className="col-6 col-lg-3 mb-3">
                         <h5>Community</h5>
                         <ul className="list-unstyled">
                             <li className="mb-2"><a href="#">Issues</a></li>
@@ -54,6 +35,23 @@ const Footer = () => {
                         </ul>
 
                     </div>
+                    <div className="col-6 col-lg-3 mb-3">
+                        <h3>Subscribe us</h3>
+                        <form>
+                            <input type="email" id="email" name="email" placeholder="Enter your email" required className="form-control" />
+                            <button type="submit" className="btn btn-primary my-2">Subscribe</button>
+                        </form>
+                        <h3>Follow us:</h3>
+                        <ul className="social-icons d-flex list-unstyled justify-content-between">
+                            <li><a href="#"><i className="fs-5 m-1 fa fa-facebook"></i></a></li>
+                            <li><a href="#"><i className="fs-5 m-1 fa fa-twitter"></i></a></li>
+                            <li><a href="#"><i className="fs-5 m-1 fa fa-instagram"></i></a></li>
+                            <li><a href="#"><i className="fs-5 m-1 fa fa-linkedin"></i></a></li>
+                            <li><a href="#"><i className="fs-5 m-1 fa fa-youtube"></i></a></li>
+                            <li><a href="#"><i className="fs-5 m-1 fa fa-pinterest"></i></a></li>
+                        </ul>
+                    </div>
+
                 </div>
             </div>
 

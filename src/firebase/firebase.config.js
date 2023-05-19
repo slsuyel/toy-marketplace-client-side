a7b0086ea38adab6e38b72c484cf96f210ac873f
Suyel Haque <slsuyel@gmail.com>
2023-05-19 23:36:13 +0600

    env file added

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -4,14 +4,22 @@ import { initializeApp } from "firebase/app";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-const firebaseConfig = {
+/* const firebaseConfig = {
   apiKey: "AIzaSyBBv_dSZX7BVK_dfvJNmwE7umnZO9Pq288",
   authDomain: "assignment-11-1f233.firebaseapp.com",
   projectId: "assignment-11-1f233",
   storageBucket: "assignment-11-1f233.appspot.com",
   messagingSenderId: "384704814473",
   appId: "1:384704814473:web:9ae95ace247e7969df5558"
-};
+}; */
+const firebaseConfig = {
+    apiKey: import.meta.env.VITE_APIKEY,
+    authDomain: import.meta.env.VITE_AUTHDOMAIN,
+    projectId: import.meta.env.VITE_PROJECTID,
+    storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+    messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+    appId: import.meta.env.VITE_APPID,
+  };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);

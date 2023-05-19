81b426656799d2fae3e2c06a090008099fb1d78b
Suyel Haque <slsuyel@gmail.com>
2023-05-19 11:52:36 +0600

    bloog added

--- src/pages/Home/Blog.jsx
+++ src/pages/Home/Blog.jsx
@@ -1,11 +1,56 @@
+import { Accordion, Container } from "react-bootstrap";
 import useTitle from "../../hooks/useTitle";
 
 const Blog = () => {
     useTitle("blog")
     return (
-        <div>
-            <h2>Blog page</h2>
-        </div>
+        <Container>
+            <div className="row">
+                <div className="col-md-6">
+                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKfePrs1BpcNT1RATNEPaoEaK11w_fwGiJTg&usqp=CAU" alt="" className="img-fluid img-thumbnail rounded-2 "
+                    />
+
+                </div>
+
+
+                <div className="col-md-6">
+
+                <Accordion defaultActiveKey="0" flush>
+    <Accordion.Item eventKey="0">
+        <Accordion.Header>Accordion Item #1</Accordion.Header>
+        <Accordion.Body>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+        </Accordion.Body>
+    </Accordion.Item>
+    <Accordion.Item eventKey="1">
+        <Accordion.Header>Accordion Item #2</Accordion.Header>
+        <Accordion.Body>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+        </Accordion.Body>
+    </Accordion.Item>
+    <Accordion.Item eventKey="2">
+        <Accordion.Header>Accordion Item #3</Accordion.Header>
+        <Accordion.Body>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+        </Accordion.Body>
+    </Accordion.Item>
+    <Accordion.Item eventKey="3">
+        <Accordion.Header>Accordion Item #4</Accordion.Header>
+        <Accordion.Body>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+        </Accordion.Body>
+    </Accordion.Item>
+
+   
+</Accordion>
+
+
+
+
+                </div>
+            </div>
+        </Container>
+
     );
 };
 
